aa54b06f59214f7b6712f0819b9d9322709c5062
Valéria Padilha de Vargas <valeria_padilha@yahoo.com.br>
2020-05-09 21:48:16 -0300

    Removed src/images

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,13 +17,6 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
         name: `posts`,
         path: `${__dirname}/posts`,
       },
