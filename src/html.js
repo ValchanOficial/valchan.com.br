e6ca42bf3b10839376977446058431e2b87a1c34
Valéria Padilha de Vargas <valeria_padilha@yahoo.com.br>
2021-12-02 22:00:15 -0300

    gtm

--- src/html.js
+++ src/html.js
@@ -11,6 +11,17 @@ export default function HTML(props) {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
+        {/* <!-- Google Tag Manager --> */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-W5WM5S2');`,
+          }}
+        />
+        {/* <!-- End Google Tag Manager --> */}
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes} className="light">
@@ -44,6 +55,10 @@ export default function HTML(props) {
         <noscript key="noscript" id="gatsby-noscript">
           This app works best with JavaScript enabled.
         </noscript>
+        {/* <!-- Google Tag Manager (noscript) --> */}
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W5WM5S2"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->
         <div
           key={`body`}
           id="___gatsby"
